d3fb76a4541828c9f79a41a1eb80c1add384b238
El Jefe <Tagne13@gmail.com>
2023-06-07 10:57:48 -0400

    added reaction and thought schemas to thought model

---
+++ models/thought.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+       reactionId: {
+
+       },
+       reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+       },
+       username: {
+            type: String,
+            required: true
+       },
+       createdAt: {
+            type: Date,
+            default: Date.now,
+            
+       } 
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
