aea1520ae6e7289a980983a45cc9c9d1953fe188
El Jefe <Tagne13@gmail.com>
2023-06-13 16:21:15 -0400

    added db connection for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api');
 
 connect(connectionString, {
   useNewUrlParser: true,
