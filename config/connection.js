badb8c16c103318e6680c3535b82857f03846af1
El Jefe <Tagne13@gmail.com>
2023-06-14 11:31:11 -0400

    added screenshot to images folder with link in README file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api');
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
 
 connect(connectionString, {
   useNewUrlParser: true,
